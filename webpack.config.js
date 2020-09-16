789c125b02266192adc42314869b7e2e37d3e701
Jonathan Noser <JnoserSEO@users.noreply.github.com>
2020-09-16 04:43:07 +0000

    setting up react redux typescript template build suceeded

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+    //entry: './src/app.js',
+    entry: './frontend/app.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public/')
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+        }, {
+            test: /\.s?css$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ],
+        },
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    mode: 'development',
+    devtool: 'inline-source-map'
+};
